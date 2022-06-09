234ed7d68c4c5b5b2321c1fa67e8f8a589277d71
5usan <susanshrestha2056@gmail.com>
2022-06-09 12:49:35 +0545

    validation added while submitting

---
+++ source/App.js
@@ -0,0 +1,7 @@
+import React, {useState} from 'react';
+import SimpleForm from './components/login';
+
+const App = () => {
+  return <SimpleForm />;
+};
+export default App;
--- source/components/login.js
+++ source/components/login.js
@@ -4,23 +4,39 @@ import {
   Text,
   StyleSheet,
   TextInput,
-  Button,
   Dimensions,
   Alert,
+  Button,
 } from 'react-native';
 
 const SimpleForm = () => {
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState();
-  console.log(email);
-  const submitHandler = () =>
-    Alert.alert('Login', `E-mail: ${email} and Password: ${password}`, [
-      {text: 'OK', onPress: () => console.log('OK Pressed')},
-    ]);
+  const [password, setPassword] = useState('');
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [errorPassword, setErrorPassword] = useState(false);
+  const changeHandler = e => {
+    console.log(e.target);
+  };
+  const submitHandler = () => {
+    if (email === '') {
+      setErrorEmail(prepState => (prepState = true));
+    }
+    if (password === '') {
+      setErrorPassword(prepState => (prepState = true));
+    } else {
+      setErrorEmail(false);
+      setErrorPassword(false);
+      Alert.alert('Login', `E-mail: ${email} and Password: ${password}`, [
+        {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ]);
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}></View>
+      <View style={styles.header}>
+        <Text style={styles.heading}>OFDesk</Text>
+      </View>
       <View style={styles.content}>
         <View style={styles.welcome}>
           <Text style={styles.hello}>Hello there, welcome back</Text>
@@ -29,17 +45,22 @@ const SimpleForm = () => {
           <TextInput
             style={styles.input}
             placeholder="E-mail"
+            placeholderTextColor="#bfbcbb"
             onChangeText={setEmail}
+            onChange={changeHandler}
           />
+          {errorEmail && <Text style={styles.error}>Invalid Email</Text>}
           <TextInput
             style={styles.input}
             placeholder="Password"
             onChangeText={setPassword}
+            placeholderTextColor="#bfbcbb"
           />
+          {errorPassword && <Text style={styles.error}>Invalid Passowrd</Text>}
         </View>
         <View>
           <Text style={styles.forget}>Forget your password?</Text>
-          <Button title="Sign In" color="#653ED6" onPress={submitHandler} />
+          <Button title="Sign In" color="#6F3CCF" onPress={submitHandler} />
         </View>
         <Text style={styles.forget}>Not here, Sign up instead</Text>
       </View>
@@ -61,6 +82,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#6F3CCF',
     height: 80,
     width: Dimensions.get('window').width,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  heading: {
+    fontSize: 40,
+    color: 'white',
   },
   content: {
     flexDirection: 'column',
@@ -89,5 +117,9 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 15,
     marginBottom: 30,
+  },
+  error: {
+    color: 'red',
+    fontSize: 15,
   },
 });
